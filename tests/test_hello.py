b4fffee688ab25974a6842e2596547d457d46971
Chris Drake <cjdrake@gmail.com>
2025-03-09 19:51:08 -0400

    Miscellaneous refactoring

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -7,6 +7,12 @@ from deltacycle import run, sleep
 logger = logging.getLogger("deltacycle")
 
 
+EXP = [
+    (2, "Hello"),
+    (4, "World"),
+]
+
+
 def test_hello(caplog):
     """Test basic async/await hello world functionality."""
     caplog.set_level(logging.INFO, logger="deltacycle")
@@ -19,8 +25,5 @@ def test_hello(caplog):
 
     run(hello())
 
-    msgs = [(r.time, r.msg) for r in caplog.records]
-    assert msgs == [
-        (2, "Hello"),
-        (4, "World"),
-    ]
+    msgs = [(r.time, r.getMessage()) for r in caplog.records]
+    assert msgs == EXP
