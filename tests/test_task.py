5e21d279a568dbc290a3d5bba2ceae5f495ff526
Chris Drake <cjdrake@gmail.com>
2025-03-09 19:58:55 -0400

    Update task logging

--- tests/test_task.py
+++ tests/test_task.py
@@ -1,93 +1,95 @@
 """Test seqlogic.sim.Lock class."""
 
+import logging
+
 import pytest
 
-from deltacycle import CancelledError, Task, TaskGroup, create_task, now, run, sleep
+from deltacycle import CancelledError, Task, TaskGroup, create_task, run, sleep
 
-
-def log(s: str):
-    print(f"{now():04} {s}")
+logger = logging.getLogger("deltacycle")
 
 
 async def basic_c1():
-    log("C1 enter")
+    logger.info("C1 enter")
 
     await sleep(10)
-    log("C1")
+    logger.info("C1")
     await sleep(10)
-    log("C1")
+    logger.info("C1")
     await sleep(10)
-    log("C1")
+    logger.info("C1")
     await sleep(10)
 
-    log("C1 exit")
+    logger.info("C1 exit")
 
 
 async def basic_c2(t: Task):
-    log("C2 enter")
+    logger.info("C2 enter")
 
-    log("C2 suspend")
+    logger.info("C2 suspend")
     await t
-    log("C2 resume")
+    logger.info("C2 resume")
 
     await t
     await sleep(10)
     await t
 
-    log("C2 exit")
+    logger.info("C2 exit")
 
 
-EXP1 = """\
-0000 C1 enter
-0000 C2 enter
-0000 C2 suspend
-0010 C1
-0020 C1
-0030 C1
-0040 C1 exit
-0040 C2 resume
-0050 C2 exit
-"""
+EXP1 = {
+    (0, "C1 enter"),
+    (0, "C2 enter"),
+    (0, "C2 suspend"),
+    (10, "C1"),
+    (20, "C1"),
+    (30, "C1"),
+    (40, "C1 exit"),
+    (40, "C2 resume"),
+    (50, "C2 exit"),
+}
 
 
-def test_basic(capsys):
+def test_basic(caplog):
+    caplog.set_level(logging.INFO, logger="deltacycle")
 
     async def main():
         t1 = create_task(basic_c1())
         create_task(basic_c2(t1))
 
     run(main())
-    assert capsys.readouterr().out == EXP1
+    msgs = {(r.time, r.getMessage()) for r in caplog.records}
+    assert msgs == EXP1
 
 
 async def cancel_c1():
-    log("C1 enter")
+    logger.info("C1 enter")
 
     try:
         await sleep(1000)
     except CancelledError:
-        log("C1 except")
+        logger.info("C1 except")
         raise
     finally:
-        log("C1 finally")
+        logger.info("C1 finally")
 
 
 async def cancel_c2():
-    log("C2 enter")
+    logger.info("C2 enter")
 
     task = create_task(cancel_c1())
 
     await sleep(1)
 
-    log("C2 cancels C1")
+    logger.info("C2 cancels C1")
     task.cancel()
 
     try:
         await task
     except CancelledError:
-        log("C2 except")
+        logger.info("C2 except")
     finally:
-        log("C2 finally")
+        logger.info("C2 finally")
 
     assert task.done()
     assert task.cancelled()
@@ -100,55 +102,59 @@ async def cancel_c2():
         task.exception()
 
 
-EXP2 = """\
-0000 C2 enter
-0000 C1 enter
-0001 C2 cancels C1
-0001 C1 except
-0001 C1 finally
-0001 C2 except
-0001 C2 finally
-"""
+EXP2 = {
+    (0, "C2 enter"),
+    (0, "C1 enter"),
+    (1, "C2 cancels C1"),
+    (1, "C1 except"),
+    (1, "C1 finally"),
+    (1, "C2 except"),
+    (1, "C2 finally"),
+}
 
 
-def test_cancel(capsys):
+def test_cancel(caplog):
+    caplog.set_level(logging.INFO, logger="deltacycle")
+
     run(cancel_c2())
-    assert capsys.readouterr().out == EXP2
+    msgs = {(r.time, r.getMessage()) for r in caplog.records}
+    assert msgs == EXP2
 
 
 async def group_c1():
-    log("C1 enter")
+    logger.info("C1 enter")
     await sleep(5)
-    log("C1 exit")
+    logger.info("C1 exit")
     return 1
 
 
 async def group_c2():
-    log("C2 enter")
+    logger.info("C2 enter")
     await sleep(10)
-    log("C2 exit")
+    logger.info("C2 exit")
     return 2
 
 
 async def group_c3():
-    log("C3 enter")
+    logger.info("C3 enter")
     await sleep(15)
-    log("C3 exit")
+    logger.info("C3 exit")
     return 3
 
 
-EXP3 = """\
-0000 C1 enter
-0000 C2 enter
-0000 C3 enter
-0005 C1 exit
-0010 C2 exit
-0015 C3 exit
-0015 MAIN
-"""
+EXP3 = {
+    (0, "C1 enter"),
+    (0, "C2 enter"),
+    (0, "C3 enter"),
+    (5, "C1 exit"),
+    (10, "C2 exit"),
+    (15, "C3 exit"),
+    (15, "MAIN"),
+}
 
 
-def test_group(capsys):
+def test_group(caplog):
+    caplog.set_level(logging.INFO, logger="deltacycle")
 
     async def main():
         async with TaskGroup() as tg:
@@ -156,11 +162,12 @@ def test_group(capsys):
             t2 = tg.create_task(group_c2())
             t3 = tg.create_task(group_c3())
 
-        log("MAIN")
+        logger.info("MAIN")
 
         assert t1.result() == 1
         assert t2.result() == 2
         assert t3.result() == 3
 
     run(main())
-    assert capsys.readouterr().out == EXP3
+    msgs = {(r.time, r.getMessage()) for r in caplog.records}
+    assert msgs == EXP3
