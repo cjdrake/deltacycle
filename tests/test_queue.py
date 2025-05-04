efd8b8b697319255fceb7bbb11acfba0bb23f267
Chris Drake <cjdrake@gmail.com>
2025-05-04 03:15:17 -0400

    Bump queue coverage

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -7,7 +7,7 @@ from deltacycle import Queue, create_task, run, sleep
 logger = logging.getLogger("deltacycle")
 
 
-EXP = {
+EXP1 = {
     (0, "Producing: 0"),
     (0, "Consuming: 0"),
     (10, "Producing: 1"),
@@ -32,7 +32,7 @@ EXP = {
 }
 
 
-def test_prod_cons(caplog):
+def test_prod_cons1(caplog):
     caplog.set_level(logging.INFO, logger="deltacycle")
 
     q = Queue()
@@ -57,4 +57,57 @@ def test_prod_cons(caplog):
     run(main())
 
     msgs = {(r.time, r.getMessage()) for r in caplog.records}
-    assert msgs == EXP
+    assert msgs == EXP1
+
+
+EXP2 = {
+    (0, "Producing: 0"),
+    (0, "Producing: 1"),
+    (0, "Producing: 2"),
+    (10, "Consuming: 0"),
+    (10, "Producing: 3"),
+    (20, "Consuming: 1"),
+    (20, "Producing: 4"),
+    (30, "Consuming: 2"),
+    (30, "Producing: 5"),
+    (40, "Consuming: 3"),
+    (40, "Producing: 6"),
+    (50, "Consuming: 4"),
+    (50, "Producing: 7"),
+    (60, "Consuming: 5"),
+    (60, "Producing: 8"),
+    (70, "Consuming: 6"),
+    (70, "Producing: 9"),
+    (80, "Consuming: 7"),
+    (80, "Producer: CLOSED"),
+    (90, "Consuming: 8"),
+    (100, "Consuming: 9"),
+}
+
+
+def test_prod_cons2(caplog):
+    caplog.set_level(logging.INFO, logger="deltacycle")
+
+    q = Queue(2)
+    assert q.maxsize == 2
+
+    async def prod():
+        for i in range(10):
+            logger.info("Producing: %d", i)
+            await q.put(i)
+        logger.info("Producer: CLOSED")
+
+    async def cons():
+        while True:
+            await sleep(10)
+            i = await q.get()
+            logger.info("Consuming: %d", i)
+
+    async def main():
+        create_task(prod())
+        create_task(cons())
+
+    run(main())
+
+    msgs = {(r.time, r.getMessage()) for r in caplog.records}
+    assert msgs == EXP2
