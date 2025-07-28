ef96545b6455ac0479be828a948de91157b94af9
Chris Drake <cjdrake@gmail.com>
2025-07-27 22:35:04 -0400

    Add a try_get check for credits

--- tests/test_credits.py
+++ tests/test_credits.py
@@ -43,7 +43,27 @@ async def use_get_put(credits: CreditPool, t1: int, t2: int):
         await sleep(t2)
     finally:
         logger.info("put")
-        credits.put(2)
+        credits.put(n=2)
+
+    await sleep(10)
+    logger.info("exit")
+
+
+async def use_try_get_put(credits: CreditPool, t1: int, t2: int):
+    logger.info("enter")
+
+    await sleep(t1)
+
+    logger.info("attempt get")
+    while not credits.try_get(n=2):
+        await sleep(1)
+    logger.info("acquired")
+
+    try:
+        await sleep(t2)
+    finally:
+        logger.info("put")
+        credits.put(n=2)
 
     await sleep(10)
     logger.info("exit")
@@ -127,6 +147,71 @@ EXP1 = {
     (44, "9", "exit"),
 }
 
+EXP2 = {
+    # 0
+    (0, "0", "enter"),
+    (10, "0", "attempt get"),
+    (10, "0", "acquired"),
+    (20, "0", "put"),
+    (30, "0", "exit"),
+    # 1
+    (0, "1", "enter"),
+    (11, "1", "attempt get"),
+    (11, "1", "acquired"),
+    (21, "1", "put"),
+    (31, "1", "exit"),
+    # 2
+    (0, "2", "enter"),
+    (12, "2", "attempt get"),
+    (12, "2", "acquired"),
+    (22, "2", "put"),
+    (32, "2", "exit"),
+    # 3
+    (0, "3", "enter"),
+    (13, "3", "attempt get"),
+    (13, "3", "acquired"),
+    (23, "3", "put"),
+    (33, "3", "exit"),
+    # 4
+    (0, "4", "enter"),
+    (14, "4", "attempt get"),
+    (14, "4", "acquired"),
+    (24, "4", "put"),
+    (34, "4", "exit"),
+    # 5
+    (0, "5", "enter"),
+    (15, "5", "attempt get"),
+    # NOTE: Ordering mismatch starts here.
+    (24, "5", "acquired"),
+    (34, "5", "put"),
+    # NOTE: But total time (44) remains the same
+    (44, "5", "exit"),
+    # 6
+    (0, "6", "enter"),
+    (16, "6", "attempt get"),
+    (23, "6", "acquired"),
+    (33, "6", "put"),
+    (43, "6", "exit"),
+    # 7
+    (0, "7", "enter"),
+    (17, "7", "attempt get"),
+    (22, "7", "acquired"),
+    (32, "7", "put"),
+    (42, "7", "exit"),
+    # 8
+    (0, "8", "enter"),
+    (18, "8", "attempt get"),
+    (21, "8", "acquired"),
+    (31, "8", "put"),
+    (41, "8", "exit"),
+    # 9
+    (0, "9", "enter"),
+    (19, "9", "attempt get"),
+    (20, "9", "acquired"),
+    (30, "9", "put"),
+    (40, "9", "exit"),
+}
+
 
 def test_get_put(caplog: LogCaptureFixture):
     caplog.set_level(logging.INFO, logger="deltacycle")
@@ -140,6 +225,20 @@ def test_get_put(caplog: LogCaptureFixture):
 
     msgs = {(r.time, r.taskName, r.getMessage()) for r in caplog.records}
     assert msgs == EXP1
+
+
+def test_try_get_put(caplog: LogCaptureFixture):
+    caplog.set_level(logging.INFO, logger="deltacycle")
+
+    async def main():
+        credits = CreditPool(10)
+        for i in range(10):
+            create_task(use_try_get_put(credits, i + 10, 10), name=f"{i}")
+
+    run(main())
+
+    msgs = {(r.time, r.taskName, r.getMessage()) for r in caplog.records}
+    assert msgs == EXP2
 
 
 def test_async_with(caplog: LogCaptureFixture):
