fe9b681b9b6845e82c0e920cd9eec3c24ec509d1
Chris Drake <cjdrake@gmail.com>
2025-07-27 21:11:33 -0400

    Improve event documentation

--- src/deltacycle/_event.py
+++ src/deltacycle/_event.py
@@ -39,7 +39,21 @@ class _WaitQ(TaskQueue):
 
 
 class Event(KernelIf, Blocking, Sendable):
-    """Notify multiple tasks that some event has happened."""
+    """Notify multiple tasks that some event has happened.
+
+    An event instance is lightweight.
+    It consists of a flag, and a FIFO of waiting tasks.
+
+    When the event is created, its flag defaults to ``False``.
+    In this state, the event will block all tasks that await it.
+    When a task invokes the event's ``set`` method,
+    that sets the flag (to ``True``), and unblocks all waiting tasks.
+
+    If the event's flag is set, it will not block awaiting tasks.
+    When a task invokes the event's ``clear`` method,
+    that clears the flag (to ``False``),
+    and the event will go back to blocking awaiting tasks.
+    """
 
     def __init__(self):
         self._flag = False
@@ -52,6 +66,7 @@ class Event(KernelIf, Blocking, Sendable):
         self._waiting.push(task)
 
     def __await__(self) -> Generator[None, Sendable, Self]:
+        """Await event set."""
         if self._blocking():
             task = self._kernel.task()
             self.wait_push(task)
@@ -60,6 +75,25 @@ class Event(KernelIf, Blocking, Sendable):
 
         return self
 
+    def __bool__(self) -> bool:
+        """Return flag state."""
+        return self._flag
+
+    def set(self):
+        """Set the flag. Stop blocking waiting tasks."""
+        self._flag = True
+        self._waiting.load()
+
+        while self._waiting:
+            task = self._waiting.pop()
+            self._kernel.join_any(task, self)
+            self._kernel.call_soon(task, args=(Task.Command.RESUME, self))
+
+    def clear(self):
+        """Clear the flag. Start blocking waiting tasks."""
+        self._flag = False
+
+    # Blocking
     def try_block(self, task: Task) -> bool:
         if self._blocking():
             self.wait_push(task)
@@ -70,20 +104,6 @@ class Event(KernelIf, Blocking, Sendable):
     def s(self) -> Event:
         return self
 
+    # Sendable
     def cancel(self, task: Task):
         self._waiting.drop(task)
-
-    def __bool__(self) -> bool:
-        return self._flag
-
-    def set(self):
-        self._flag = True
-        self._waiting.load()
-
-        while self._waiting:
-            task = self._waiting.pop()
-            self._kernel.join_any(task, self)
-            self._kernel.call_soon(task, args=(Task.Command.RESUME, self))
-
-    def clear(self):
-        self._flag = False
