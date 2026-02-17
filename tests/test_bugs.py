f364f317eff34ea01e31c9ea5edcb2dcefc09195
Chris Drake <cjdrake@gmail.com>
2026-02-16 21:38:05 -0500

    Use captrace w/ test_bugs

--- tests/test_bugs.py
+++ tests/test_bugs.py
@@ -1,31 +1,30 @@
 """Test bugs"""
 
-from pytest import CaptureFixture
-
 from deltacycle import TaskGroup, run, sleep
 
-from .common import Bool, ttprint
+from .common import Bool
+from .conftest import trace
 
-EXP2 = """\
-[   5][do_stuff] first
-[  15][do_stuff] second
-[  25][do_stuff] third
-[  35][do_stuff] fourth
-"""
+EXP2 = {
+    (5, "do_stuff", "first"),
+    (15, "do_stuff", "second"),
+    (25, "do_stuff", "third"),
+    (35, "do_stuff", "fourth"),
+}
 
 
-def test_2(capsys: CaptureFixture[str]):
+def test_2(captrace: set[tuple[int, str, str]]):
     clock = Bool(name="clock")
 
     async def do_stuff():
         await clock.posedge()
-        ttprint("first")
+        trace("first")
         await clock.posedge()
-        ttprint("second")
+        trace("second")
         await clock.posedge()
-        ttprint("third")
+        trace("third")
         await clock.posedge()
-        ttprint("fourth")
+        trace("fourth")
 
     async def drv_clock():
         clock.next = False
@@ -40,7 +39,4 @@ def test_2(capsys: CaptureFixture[str]):
 
     run(main(), until=100)
 
-    cap = capsys.readouterr()
-    # msgs = {(r.time, r.taskName, r.getMessage()) for r in caplog.records}
-    # assert msgs == EXP2
-    assert cap.out == EXP2
+    assert captrace == EXP2
