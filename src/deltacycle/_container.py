d3da9b180b0f56b5bea4609bcf7762e320be0a6a
Chris Drake <cjdrake@gmail.com>
2026-08-01 23:14:58 -0400

    Miscellaneous refactoring

--- src/deltacycle/_container.py
+++ src/deltacycle/_container.py
@@ -50,6 +50,12 @@ class Container(KernelIf):
         if self._has_capacity and n > self._capacity:
             raise ValueError(f"Expected n ≤ {self._capacity}, got {n}")
 
+    def _empty(self, n: int) -> bool:
+        return self._cnt < n
+
+    def _full(self, n: int) -> bool:
+        return self._has_capacity and (self._cnt + n) > self._capacity
+
     def _getq_pop(self) -> tuple[Task[Any], int]:
         task, n = self._getq.pop()
         self._kernel.call_soon(task, args=(Task.Command.RESUME,))
@@ -60,60 +66,62 @@ class Container(KernelIf):
         self._kernel.call_soon(task, args=(Task.Command.RESUME,))
         return task, n
 
+    def _put(self, n: int):
+        self._cnt += n
+
     def try_put(self, n: int = 1) -> bool:
         self._check_cnt()
         self._check_n(n)
 
-        if self._has_capacity and (self._cnt + n) > self._capacity:
+        if self._full(n):
             return False
 
-        # Put credit
-        self._cnt += n
+        self._put(n)
         return True
 
     async def put(self, n: int = 1, priority: int = 0):
         self._check_cnt()
         self._check_n(n)
 
-        if self._has_capacity and (self._cnt + n) > self._capacity:
+        if self._full(n):
             task = self._kernel.check_task()
             self._putq.push(priority, task, n)
             y = await task.switch_coro()
             assert y is None
         else:
-            # Put credit
-            self._cnt += n
+            self._put(n)
 
         while self._getq and (self._cnt >= self._getq.peek()):
             # Transfer credit
             _, n = self._getq_pop()
-            self._cnt -= n
+            self._get(n)
+
+    def _get(self, n: int):
+        self._cnt -= n
 
     def try_get(self, n: int = 1) -> bool:
         self._check_cnt()
         self._check_n(n)
 
-        if self._cnt < n:
+        if self._empty(n):
             return False
 
-        # Get credit
-        self._cnt -= n
+        self._get(n)
         return True
 
     async def get(self, n: int = 1, priority: int = 0):
         self._check_cnt()
         self._check_n(n)
 
-        if self._cnt < n:
+        if self._empty(n):
             task = self._kernel.check_task()
             self._getq.push(priority, task, n)
             y = await task.switch_coro()
             assert y is None
         else:
-            # Get credit
-            self._cnt -= n
+            self._get(n)
 
         while self._putq and (self._cnt + self._putq.peek()) <= self._capacity:
             # Transfer credit
             _, n = self._putq_pop()
-            self._cnt += n
+            self._put(n)
